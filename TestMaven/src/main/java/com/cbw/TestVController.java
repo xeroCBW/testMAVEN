4e88bb01523ef2db6f144a846ba8980a4d14c2f7
DESKTOP-DJCQ2A6\cbw <861754186@qq.com>
2017-12-19 10:51:11 +0800

    this is test 03

--- TestMaven/src/main/java/com/cbw/TestVController.java
+++ TestMaven/src/main/java/com/cbw/TestVController.java
@@ -6,36 +6,37 @@ package com.cbw;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**   
-* @Title: TestVController.java 
-* @Package com.cbw 
-* @Description: TODO(用一句话描述该文件做什么) 
-* @author cbw   
-* @date 2017年12月18日 下午7:06:37 
-* @version V1.0   
-*/
+/**
+ * @Title: TestVController.java
+ * @Package com.cbw
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @author cbw
+ * @date 2017年12月18日 下午7:06:37
+ * @version V1.0
+ */
 
 @RestController
 @RequestMapping("/test")
 public class TestVController {
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
-		
-		
-		
-		
+
 		return "hello";
-		
-		
+
 	}
-	
+
 	@RequestMapping("/hello01")
 	public String test02() {
-		
-		
+
 		return "this is test 02";
 	}
 	
+	
+	@RequestMapping("hello02")
+	public String test03() {
+		
+		return "this is test 03";
+	}
 
 }
